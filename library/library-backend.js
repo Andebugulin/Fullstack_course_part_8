f1bcd84c89c2704f4db034267e6f0e9a808bf4ca
Andebugulin <gulin.andrey2005@gmail.com>
2025-07-09 15:15:20 +0300

    exercise 8.3

--- library/library-backend.js
+++ library/library-backend.js
@@ -117,6 +117,7 @@ const typeDefs = `
     name: String!
     id: ID!
     born: Int
+    bookCount: Int!
   }
 
   type Book {
@@ -130,6 +131,7 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -138,6 +140,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter(book => book.author === root.name).length
   }
 }
 
